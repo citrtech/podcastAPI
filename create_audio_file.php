f15ee6ad814a5b30e59c07734838efe22f354ab9
brad winter <braddjwinter@gmail.com>
2015-03-04 15:52:12 -0800

    ftp

--- create_audio_file.php
+++ create_audio_file.php
@@ -34,8 +34,8 @@ if(!is_writeable( $audio_dir )) {
 }
 if($error == ''){
 	$new_podcast_audio_file = file_get_contents($archive_url); // use to test:  file_get_contents('http://maltinerecords.cs8.biz/111/03.mp3');
-	
-	if(strlen($new_podcast_audio_file) <=0 ){	
+
+	if(strlen($new_podcast_audio_file) <=0 ){
 		$error .=  'Cannot retrieve audio from archiver at '.$archive_url.'.  ';
 	}
 
@@ -44,7 +44,7 @@ if($error == ''){
 if($error == ''){
 	$file_name = $show.'-'.$start.'-'.$end.'.mp3';
 	$num_bytes = file_put_contents($audio_dir.$file_name,$new_podcast_audio_file);
-				
+
 	if ($num_bytes > 0){
 		//SUCCESS
 		} else {
